a9a18bbbc304126df5849a3f31ca166f5598a187
linqueta <lincolnrodrs@gmail.com>
2019-05-20 22:00:16 -0300

    add author model and relations between author and book

---
+++ db/migrate/20190521005059_add_author_to_book.rb
@@ -0,0 +1,5 @@
+class AddAuthorToBook < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :books, :author, foreign_key: true, index: true, type: :uuid
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_161117) do
+ActiveRecord::Schema.define(version: 2019_05_21_005059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
-  create_table "books", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "title"
+  create_table "authors", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
